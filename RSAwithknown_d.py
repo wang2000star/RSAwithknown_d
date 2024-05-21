28ce64a61a23a986bb9905d01351633babf0b2c3
wfzChina <wangfangzhen23@mails.ucas.ac.cn>
2024-05-21 21:35:36 +0800

    Update RSAwithknown_d.py

--- RSAwithknown_d.py
+++ RSAwithknown_d.py
@@ -27,19 +27,25 @@ def is_perfect_square(n):
 def factor_pqRange(n, e, d):
     """
         Pricinple: we suppose q<p<Lq, acutually L always be 2;
-        p+q>2sqrt(pq),p+q=sqrt((p-q)^2+4pq)<sqrt((L+3)pq)
+        p+q>2sqrt(pq),p+q=sqrt((p-q)^2+4pq)<sqrt(pq(L+1)^2/L)
         phi=pq-(p+q)+1,k=(ed-1)/phi,
         max(e,d)<phi,1<=k<min(e,d)
-        so 1+a//A<=k<=min{e,d,1+b//B}
+        so 1+a//A<=k<=min{e,d,1+b//B},where"//"denotes floor division.
+        In addition, it is easy to find a simple corollary, when ed <= 16*n^(3/2), a//A = b//B. 
+        Specifically, when min(e,d) <= 16*n^(1/2), it is obvious that a//A = b//B.
     """
     W = e*d - 1
     a = (n + 1)*W + gmpy2.isqrt(4*n*W**2)
     A = pow(n - 1,2)
     L = 2
-    b = (n + 1)*W + gmpy2.isqrt((L+3)*n*W**2)
-    B = pow(n + 1,2) - 5*n
+    b = L*(n + 1)*W + int(gmpy2.sqrt(L**3*(L+1)^2*n*W**2)/2)
+    B = L*pow(n + 1,2) - (L+1)**2*n
     kmin = 1 + a//A
-    kmax = min(min(e, d) - 1, 1 + b//B)
+    ed_min = min(e,d)
+    if ed_min**2 <= 64*n | W**2<=64*n**3:
+        kmax = kmin
+    else:
+        kmax = min(ed_min - 1, 1 + b//B)
 
     for k in range(kmin, kmax+1):#range is [kmin,kmax]
         if W % k == 0:
@@ -57,15 +63,26 @@ def factor_pqRange(n, e, d):
 #the second method to factorize n with known e and d
 def factor_pq(n, e, d):        
     """
-        Principle: Let k=ed-1=2^s*t, where k is an even number, s>=1, t>=1. Randomly select an integer g, 1<g<n-1.
-        Case 1: If gcd(g,n)!=1, then p=gcd(g,n) is a factor of n.
-        Case 2: If gcd(g,n)=1, for 0<=i<=s, the values of (g^t)^(2^i) mod n fall into four categories:
-        111111; -111111; ***-111111; ***111111, where * represents !=1 and !=n-1.
-        # For the first three categories, reselect g.
-        For the fourth category, if x^2 mod n = 1, then n|(x+1)(x-1), 1<=x<=n-1, -1<=x^2-1<=n(n-2);
-        If x!=1 and x!=n-1, then 2<x+1<n, 0<x-1<n-2, so 1<=gcd(x-1,n), gcd(x+1,n)<n.
-        Suppose gcd(x-1,n)=1, then n|x+1, contradiction; similarly, suppose gcd(x+1,n)=1, then n|x-1, contradiction.
-        The factors of n are only 1, p, q, n, so gcd(x-1,n)=p or q and gcd(x+1,n)=p or q.
+    Factorizes the given RSA modulus (n) using the known private exponent (d) and public exponent (e).
+
+    Principle: Let k=ed-1=2^s*t, where k is an even number, s>=1, t>=1. Randomly select an integer g, 1<g<n-1.
+    Case 1: If gcd(g,n)!=1, then p=gcd(g,n) is a factor of n.
+    Case 2: If gcd(g,n)=1, for 0<=i<=s, the values of (g^t)^(2^i) mod n fall into four categories:
+    111111; -111111; ***-111111; ***111111, where * represents !=1 and !=n-1.
+    For the first three categories, reselect g.
+    For the fourth category, if x^2 mod n = 1, then n|(x+1)(x-1), 1<=x<=n-1, -1<=x^2-1<=n(n-2);
+    If x!=1 and x!=n-1, then 2<x+1<n, 0<x-1<n-2, so 1<=gcd(x-1,n), gcd(x+1,n)<n.
+    Suppose gcd(x-1,n)=1, then n|x+1, contradiction; similarly, suppose gcd(x+1,n)=1, then n|x-1, contradiction.
+    The factors of n are only 1, p, q, n, so gcd(x-1,n)=p or q and gcd(x+1,n)=p or q.
+
+    Args:
+        n (int): The RSA modulus.
+        e (int): The public exponent.
+        d (int): The private exponent.
+
+    Returns:
+        tuple: A tuple containing the factors of n.
+
     """
     k = e*d - 1
     t=k
@@ -93,14 +110,14 @@ def factor_pq(n, e, d):
                         return (n//p, p)
                     if y==n-1:
                         break
-                    x=y                      
+                    x=y
 
 if __name__ == "__main__":
-    n=0xd231f2c194d3971821984dec9cf1ef58d538975f189045ef8a706f6165aab4929096f61a3eb7dd8021bf3fdc41fe3b3b0e4ecc579b4b5e7e035ffcc383436c9656533949881dca67c26d0e770e4bf62a09718dbabc2b40f2938f16327e347f187485aa48b044432e82f5371c08f6e0bbde46c713859aec715e2a2ca66574f3eb
-    e=0x5b5961921a49e3089262761e89629ab6dff2da1504a0e5eba1bb7b20d63c785a013fd6d9e021c01baf1b23830954d488041b92bca2fe2c92e3373dedd7e625da11275f6f18ee4aef336d0637505545f70f805902ddbacb21bb8276d34a0f6dfe37ede87dd95bb1494dbb5763639ba3984240f1178e32aa36ee3c5fcc8115dde5
-    d=0x31df315ae64639876342f0ce3367a436ad9b92201a450b0d8b1465786a53a5e01bd70025
-    p,q=factor_pqRange(n,e,d)
-    if p*q==n:
+    n = 0xd231f2c194d3971821984dec9cf1ef58d538975f189045ef8a706f6165aab4929096f61a3eb7dd8021bf3fdc41fe3b3b0e4ecc579b4b5e7e035ffcc383436c9656533949881dca67c26d0e770e4bf62a09718dbabc2b40f2938f16327e347f187485aa48b044432e82f5371c08f6e0bbde46c713859aec715e2a2ca66574f3eb
+    e = 0x5b5961921a49e3089262761e89629ab6dff2da1504a0e5eba1bb7b20d63c785a013fd6d9e021c01baf1b23830954d488041b92bca2fe2c92e3373dedd7e625da11275f6f18ee4aef336d0637505545f70f805902ddbacb21bb8276d34a0f6dfe37ede87dd95bb1494dbb5763639ba3984240f1178e32aa36ee3c5fcc8115dde5
+    d = 0x31df315ae64639876342f0ce3367a436ad9b92201a450b0d8b1465786a53a5e01bd70025
+    p,q = factor_pqRange(n,e,d)
+    if p*q == n:
         print("sucess!")
         print("p:",hex(p))
         print("q:",hex(q))
